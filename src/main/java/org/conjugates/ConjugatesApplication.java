eac679a4dd0df29758df23094bbf54199fd3c894
Kevin Richards <kevinlr5@gmail.com>
2017-01-10 21:10:03 -0800

    Very basic program setup
    To come: Adding in dependencies for generating documentation,
    security, proxies, JWT, etc

---
+++ src/main/java/org/conjugates/ConjugatesApplication.java
@@ -0,0 +1,12 @@
+package org.conjugates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConjugatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConjugatesApplication.class, args);
+	}
+}
---
+++ src/test/java/org/conjugates/ConjugatesApplicationTests.java
@@ -0,0 +1,16 @@
+package org.conjugates;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConjugatesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
