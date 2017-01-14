09ff392f7be7bfbbedba3264e6567921e50c5371
Kevin Richards <kevinlr5@gmail.com>
2017-01-14 15:03:01 -0800

    Validate logging works through slf4j

--- src/main/java/org/conjugates/analyzer/endpoints/info/InfoController.java
+++ src/main/java/org/conjugates/analyzer/endpoints/info/InfoController.java
@@ -1,6 +1,8 @@
 package org.conjugates.analyzer.endpoints.info;
 
 import org.conjugates.analyzer.services.properties.AnalyzerProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/info")
 @RestController
 public class InfoController {
+
+  private static final Logger LOG = LoggerFactory.getLogger(InfoController.class);
 
   private final AnalyzerProperties properties;
 
@@ -18,6 +22,7 @@ public class InfoController {
 
   @RequestMapping("/")
   public String name() {
+    LOG.debug("Call to info made");
     return properties.getName();
   }
 
