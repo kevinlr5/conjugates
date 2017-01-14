708a09d092629d5cd531a28e57cd3de07f6734df
Kevin Richards <kevinlr5@gmail.com>
2017-01-14 13:43:13 -0800

    Add google checkstyle rules to project

--- src/main/java/org/conjugates/ConjugatesApplication.java
+++ src/main/java/org/conjugates/ConjugatesApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConjugatesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConjugatesApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ConjugatesApplication.class, args);
+  }
 }
